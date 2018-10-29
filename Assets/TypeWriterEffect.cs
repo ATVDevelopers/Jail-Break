9e026db82c96262724af183f4a93f46033ad3b37
Aadar Gupta <aadar100@gmail.com>
2018-10-28 20:28:12 -0400

    Jail Break
    This is a platformer game that is in progess (being worked on by ATV: Aadar, Talat, Varchas)

---
+++ Assets/TypeWriterEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriterEffect : MonoBehaviour {
+
+    public AudioSource key;
+
+    public float delay = 0.2f;
+    public string fullText;
+    private string currentText = "";
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ShowText());
+
+	}
+	
+    IEnumerator ShowText(){
+        for (int i = 0; i <= fullText.Length; i++){
+            currentText = fullText.Substring(0, i);
+            Debug.Log("Work");
+            key.Play();
+            Debug.Log("Sound");
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
